2d5ef5280938f0493ab1438f99d50b184f55416e
Martin Kamleithner <martin.kamleithner@gmail.com>
2023-12-18 17:16:33 +0100

    example with 100 queries

---
+++ lib/graphql/copy.py
@@ -0,0 +1,127 @@
+# The user wants to create 100 copies of the given GraphQL query file with modified file names and query names.
+# We'll create a script that takes the original query, modifies it, and saves it as new files.
+
+# Define the original query
+original_query = """
+query timelineQuery($after: String, $first: Int!) {
+  timelineQuery(after: $after, first: $first) {
+    __typename
+    items {
+      __typename
+      # 8ms
+      ... on A1 {
+        id
+        a
+        b
+        c
+        d
+        e
+        f
+        g
+        h
+      }
+      # 10ms
+      ... on A2 {
+        id
+        a
+        b
+        c
+        d
+        e
+        f
+        g
+        h
+      }
+      # 11ms
+      ... on A3 {
+        id
+        a
+        b
+        c
+        d
+        e
+        f
+        g
+        h
+      }
+      # 12ms
+      ... on A4 {
+        id
+        a
+        b
+        c
+        d
+        e
+        f
+        g
+        h
+      }
+      # 21ms
+      ... on A5 {
+        id
+        a
+        b
+        c
+        d
+        e
+        f
+        g
+        h
+      }
+      # 39ms
+      ... on A6 {
+        id
+        a
+        b
+        c
+        d
+        e
+        f
+        g
+        h
+      }
+      # 131ms
+      ... on A7 {
+        id
+        a
+        b
+        c
+        d
+        e
+        f
+        g
+        h
+      }
+      # 1159ms
+      ... on A8 {
+        id
+        a
+        b
+        c
+        d
+        e
+        f
+        g
+        h
+      }
+    }
+  }
+}
+"""
+
+# Script to create modified copies
+def create_copies(original_query, num_copies):
+    for i in range(1, num_copies + 1):
+        # Replace the query name
+        modified_query = original_query.replace("query timelineQuery", f"query timelineQuery{i}")
+
+        # Save the modified query to a file
+        file_name = f"timelineQuery{i}.graphql"
+        with open(file_name, "w") as file:
+            file.write(modified_query)
+
+# Execute the function to create 100 copies
+create_copies(original_query, 1000)
+
+# Since file operations cannot be performed in this environment, this script is for demonstration.
+# You can run this script in your local environment to create the files.
